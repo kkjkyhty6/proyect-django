af3f97100de15c0b96dafc2bd12bdb9cd525fb8f
hola <107812235+kkjkyhty6@users.noreply.github.com>
2022-10-06 09:45:39 -0400

    proyect
    codigos

---
+++ scripts.js
@@ -0,0 +1,244 @@
+<script> 
+ 
+ 
+ function enviar() {
+ var formulario = document.getElementById("panel")
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+}
+function enviar() {
+ var formulario = document.getElementById("notificaciones");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+}
+function enviar() {
+ var formulario = document.getElementById("pedidos"");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+}
+function enviar() {
+ var formulario = document.getElementById("cuenta");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+}
+function enviar() {
+ var formulario = document.getElementById("calculadora");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+}
+function enviar() {
+ var formulario = document.getElementById("creditos");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+} 
+function enviar() {
+ var formulario = document.getElementById("analisis dafo");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+}
+function enviar() {
+ var formulario = document.getElementById("formato");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ 
+ function enviar() {
+ var formulario = document.getElementById("tipo");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("categoria");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("nombre del proyecto"");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Extensión máxima de palabras<");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Países a los que está dirigidoo");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Idioma de tu pedido">");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Optimización SEO");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("alabra clave principal"");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Palabra(s) semántica(s) o secundaria(s)");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Público objetivo");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+ function enviar() {
+ var formulario = document.getElementById("Perspectiva del texto");
+ var dato = formulario[];
+ if (dato.value=="enviar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ }
+} 
+function enviar() {
+ var formulario = document.getElementById("Fecha de entrega ");
+ var dato = formulario[];
+ if (dato.value=="calendar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ 
+ function enviar() {
+ var formulario = document.getElementById("crear pedido ");
+ var dato = formulario[];
+ if (dato.value=="calendar") {
+ alert("Enviando el formulario");
+ formulario.submit();
+ return true;
+ } else {
+ alert("No se envía el formulario");
+ return false;
+ 
+ </script>
