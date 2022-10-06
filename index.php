73c4242338c4ce0b5d32a200d9fdff71c5c67ae6
hola <107812235+kkjkyhty6@users.noreply.github.com>
2022-10-06 10:26:15 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<h1> muestra <h1>   <?php 
+
+foreach ( $model as $data )
+<h1> ?php echo $data => username ;?> </h1>    
+
+
+
+<?php endoforech ;?>
+
+
+
+
---
+++ users.php
@@ -0,0 +1,16 @@
+?php
+class proyecto1controller extends controller
+{
+ public function actionindex()
+$modles => CAactiverecord :models("userrs") =>find all(),
+$opciones=" requerimeintos "
+$noticaciones =>"no tiene notificaciones" ,
+$cuentas=>"2 disponibles"
+$creditos=>"no tiene"
+$analisis dofa=>"iniciar"
+$calculadora=>"ingrear"
+$this=>render (index (array ( "opciones =>opciones ,"noticaciones" =>notificaciones , 
+"cuentas"=>cuentas , "creditos" =>creditos , "calculadora"=> calculadora )
+}
+?php 
+
