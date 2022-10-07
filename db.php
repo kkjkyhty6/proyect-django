fd374cb057cb4c4d2970df12fef0084e4bd4ac00
hola <107812235+kkjkyhty6@users.noreply.github.com>
2022-10-07 04:26:29 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,23 @@
+<?php
+
+class users extendens cACtiverecords{
+
+       public function  table name( )
+       return
+
+
+}
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
