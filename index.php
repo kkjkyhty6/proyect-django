294061142495e13f23cd5b4bd77b88578ceafcb4
hola <107812235+kkjkyhty6@users.noreply.github.com>
2022-10-08 01:40:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,21 +10,5 @@ $creditos=>"no tiene"
 $analisis dofa=>"iniciar"
 $calculadora=>"ingrear"
 $this=>render (index (array ( "opciones =>opciones ,"noticaciones" =>notificaciones , 
-"cuentas"=>cuentas , "creditos" =>creditos , "calculadora"=> calculadora )
-}
-?php ?
-php
-class proyecto1controller extends controller
-{
- public function actionindex()
-$modles => CAactiverecord :models("users") =>find all(),
-$opciones=" requerimeintos "
-$noticaciones =>"no tiene notificaciones" ,
-$cuentas=>"2 disponibles"
-$creditos=>"no tiene"
-$analisis dofa=>"iniciar"
-$calculadora=>"ingrear"
-$this=>render (index (array ( "opciones =>opciones ,"noticaciones" =>notificaciones , 
-"cuentas"=>cuentas , "creditos" =>creditos , "calculadora"=> calculadora )
-}
+"cuentas"=>cuentas , "creditos" =>creditos , "calculadora"=> calculadoora),
 ?php
